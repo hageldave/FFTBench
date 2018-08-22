20774ebc95e979c476d20ad47999246451005272
David <david@RECHENKNECHTLNX>
2018-08-22 19:48:44 +0200

    added 2d benchmark

--- src/main/java/com/github/hageldave/fftbenchmark/FFT2dBenchmark.java
+++ src/main/java/com/github/hageldave/fftbenchmark/FFT2dBenchmark.java
@@ -31,7 +31,6 @@
 
 package com.github.hageldave.fftbenchmark;
 
-import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
 
@@ -46,25 +45,25 @@ import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.Warmup;
 
-import com.github.hageldave.fftbenchmark.interfaces.FFT1D;
+import com.github.hageldave.fftbenchmark.interfaces.FFT2D;
 
 @State(Scope.Benchmark)
 @BenchmarkMode(Mode.AverageTime)
 @Measurement(iterations = 3, time = 10, timeUnit = TimeUnit.SECONDS)
 @Warmup(iterations = 2, time = 10, timeUnit = TimeUnit.SECONDS)
 @Fork(value=1)
-public class FFT1dBenchmark {
+public class FFT2dBenchmark {
 
-	private static final int NUM_THREADS = 16;
+	private static final int NUM_THREADS = 8;
 	
 	@Param({
-		"512",
-		"1536",
-		"16384",
-		"98304",
-		"589829"
+		"512x512",
+		"1280x720",
+		"1920x1080",
+		"1087x1931",
+		"4928x3264"
 	})
-	public int size=0;
+	public String dims="";
 	
 	@Param({
 		"JTransforms",
@@ -72,55 +71,52 @@ public class FFT1dBenchmark {
 	})
 	public String implementation="";
 	
-	private FFT1D fft_test;
+	private int width=0;
+	private int height=0;
+	private int size=0;
+	private FFT2D fft_test;
 	private double[] toTransform;
 	private double[] result;
-	private ArrayList<Double> listToTransform;
-	private ArrayList<Double> listResult;
-	private double[][] multipleToTransform;
-	private double[][] multipleResult;
+	private double[][] toTransformNested;
+	private double[][] resultNested;
+	private double[][] multipleResults;
 
 	@Setup
 	public void setup(){
-		fft_test = Implementations.valueOf(implementation).fft1d;
+		fft_test = Implementations.valueOf(implementation).fft2d;
+		String[] dimstr = dims.split("x");
+		width = Integer.parseInt(dimstr[0]);
+		height = Integer.parseInt(dimstr[1]);
+		size = width*height;
+		
 		toTransform = new double[size];
 		result = new double[size];
-		listToTransform = new ArrayList<>(size*2/3);
-		listResult = new ArrayList<>(size*2/3);
-		multipleToTransform = new double[NUM_THREADS][size];
-		multipleResult = new double[NUM_THREADS][size];
-		for(int i=0;i<size;i++){
-			toTransform[i] = i*0.01+Math.sin(i);
-			listToTransform.add(toTransform[i]);
-			for(int j=0;j<NUM_THREADS;j++){
-				multipleToTransform[j][i] = toTransform[i];
-			}
-		}
+		
+		toTransformNested = new double[height][width];
+		resultNested = new double[height][width];
+		
+		multipleResults = new double[NUM_THREADS][size];
 	}
 
 	@Benchmark
-	public double arrayInput() {
-		fft_test.doubleArrayFFT_SetDC2Zero_1D(toTransform, result);
+	public double singleArrayInput() {
+		fft_test.doubleArrayFFT_SetDC2Zero_2D(toTransform, result, width, height);
 		return result[size-1];
 	}
 	
 	@Benchmark
-	public double listInput() {
-		try{
-			fft_test.doubleListFFT_SetDC2Zero_1D(listToTransform, listResult);
-			return listResult.get(size-1);
-		} finally {
-			listResult.clear();
-		}
+	public double nestedArrayInput() {
+		fft_test.double2DArrayFFT_SetDC2Zero_2D(toTransformNested, resultNested, width, height);
+		return resultNested[0][0];
 	}
 	
 	@Benchmark
 	public double parallelInvocations() {
-		fft_test.doubleArrayFFT_SetDC2Zero_1D(toTransform, result);
+		fft_test.doubleArrayFFT_SetDC2Zero_2D(toTransform, result, width, height);
 		IntStream.range(0, NUM_THREADS).parallel().forEach((int i)->{
-			fft_test.doubleArrayFFT_SetDC2Zero_1D(multipleToTransform[i], multipleResult[i]);
+			fft_test.doubleArrayFFT_SetDC2Zero_2D(toTransform, multipleResults[i], width, height);
 		});
-		return multipleResult[NUM_THREADS/2][size-1];
+		return multipleResults[NUM_THREADS/2][size-1];
 	}
 
 }
